d54155ffbe654ae62b4690824479b91ffced94bf
Klainis <136218846+Klainis@users.noreply.github.com>
2025-01-12 17:47:26 +0500

    .

--- Assets/Scripts/TowerSpot.cs
+++ Assets/Scripts/TowerSpot.cs
@@ -1,31 +1,90 @@
+//using UnityEngine;
+
+//public class TowerSpot : MonoBehaviour
+//{
+//    private bool isOccupied;
+
+//    public bool IsOccupied => isOccupied;
+
+//    public void SetOccupied(bool occupied)
+//    {
+//        isOccupied = occupied;
+//    }
+
+//    public void OnMouseDown()
+//    {
+//        if (isOccupied)
+//        {
+//            Debug.LogWarning("This spot is already occupied!");
+//            return;
+//        }
+
+//        if (TowerManager.Instance != null && TowerManager.Instance.HasSelectedTower())//HasSelectedTower
+//        {
+//            TowerManager.Instance.TryPlaceTower(transform.position, this);
+//        }
+//        else
+//        {
+//            Debug.LogWarning("No tower selected or invalid spot!");
+//        }
+//    }
+//}
 using UnityEngine;
 
 public class TowerSpot : MonoBehaviour
 {
-    private bool isOccupied;
+    [SerializeField] private bool isOccupied = false;
+    private Renderer spotRenderer;
 
     public bool IsOccupied => isOccupied;
 
-    public void SetOccupied(bool occupied)
+    private void Awake()
     {
-        isOccupied = occupied;
+        spotRenderer = GetComponent<Renderer>();
     }
 
-    public void OnMouseDown()
+    public void SetOccupied(bool state)
     {
-        if (isOccupied)
-        {
-            Debug.LogWarning("This spot is already occupied!");
-            return;
-        }
+        isOccupied = state;
+    }
 
-        if (TowerManager.Instance != null && TowerManager.Instance.HasSelectedTower())
+    private void OnMouseEnter()
+    {
+        Debug.Log("Mouse entered TowerSpot");
+        if (!isOccupied && TowerManager.Instance.HasSelectedTower())
         {
-            TowerManager.Instance.TryPlaceTower(transform.position, this);
+            // Подсвечиваем место для размещения
+            HighlightSpot(true);
         }
-        else
+        // Показываем превью выбранной башни
+        TowerManager.Instance.ShowPreview(transform.position);
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.Log("Mouse exit TowerSpot");
+        // Убираем подсветку
+        HighlightSpot(false);
+        TowerManager.Instance.HidePreview();
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Mouse clicked!");
+        if (!isOccupied && TowerManager.Instance.HasSelectedTower())
         {
-            Debug.LogWarning("No tower selected or invalid spot!");
+            // Логика размещения башни обрабатывается в TowerManager
+            TowerManager.Instance.TryPlaceTower();
+        }
+    }
+
+    private void HighlightSpot(bool highlight)
+    {
+        if (spotRenderer != null)
+        {
+            Debug.Log("Color changed");
+            // Можно настроить цвет подсветки здесь
+            spotRenderer.material.color = highlight ? Color.green : Color.white;
         }
     }
 }
