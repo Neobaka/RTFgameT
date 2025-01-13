aef62634c1c71b95dca43fd4908057a5a231d8ba
Klainis <136218846+Klainis@users.noreply.github.com>
2025-01-13 16:59:00 +0500

    Добавил размещение башен

--- Assets/Scripts/TowerButton.cs
+++ Assets/Scripts/TowerButton.cs
@@ -1,29 +1,29 @@
-using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
+//using UnityEngine;
+//using UnityEngine.UI;
+//using TMPro;
 
 
-public class TowerButton : MonoBehaviour
-{
-    [SerializeField] private Image iconImage;
-    [SerializeField] private TextMeshProUGUI costText;
+//public class TowerButton : MonoBehaviour
+//{
+//    [SerializeField] private Image iconImage;
+//    [SerializeField] private TextMeshProUGUI costText;
 
-    private TowerManager.TowerOption towerOption;
+//    private TowerManager.TowerOption towerOption;
 
-    public void Initialize(TowerManager.TowerOption option)
-    {
-        towerOption = option;
+//    public void Initialize(TowerManager.TowerOption option)
+//    {
+//        towerOption = option;
 
-        // Устанавливаем данные кнопки
-        iconImage.sprite = option.icon;
-        costText.text = option.cost.ToString();
-    }
+//        // Устанавливаем данные кнопки
+//        iconImage.sprite = option.icon;
+//        costText.text = option.cost.ToString();
+//    }
 
-    public void OnClick()
-    {
-        if (TowerManager.Instance != null)
-        {
-            TowerManager.Instance.SelectTower(towerOption);
-        }
-    }
-}
+//    public void OnClick()
+//    {
+//        if (TowerManager.Instance != null)
+//        {
+//            TowerManager.Instance.SelectTower(towerOption);
+//        }
+//    }
+//}
--- Assets/Scripts/TowerSpot.cs
+++ Assets/Scripts/TowerSpot.cs
@@ -2,30 +2,70 @@
 
 //public class TowerSpot : MonoBehaviour
 //{
-//    private bool isOccupied;
+//    [SerializeField] private bool isOccupied = false;
+//    private SpriteRenderer spotRenderer;
 
 //    public bool IsOccupied => isOccupied;
 
-//    public void SetOccupied(bool occupied)
+//    private void Awake()
 //    {
-//        isOccupied = occupied;
+//        spotRenderer = GetComponent<SpriteRenderer>();
 //    }
 
-//    public void OnMouseDown()
+//    public void SetOccupied(bool state)
 //    {
-//        if (isOccupied)
-//        {
-//            Debug.LogWarning("This spot is already occupied!");
-//            return;
-//        }
+//        isOccupied = state;
+//    }
 
-//        if (TowerManager.Instance != null && TowerManager.Instance.HasSelectedTower())//HasSelectedTower
+//    private void OnMouseEnter()
+//    {
+//        Debug.Log("Mouse entered TowerSpot");
+//        if (!isOccupied && TowerManager.Instance.HasSelectedTower())
 //        {
-//            TowerManager.Instance.TryPlaceTower(transform.position, this);
+//            // Подсвечиваем место для размещения
+//            HighlightSpot(true);
 //        }
-//        else
+//        // Показываем превью выбранной башни
+//        TowerManager.Instance.ShowPreview(transform.position);
+//    }
+
+//    private void OnMouseExit()
+//    {
+//        Debug.Log("Mouse exit TowerSpot");
+//        // Убираем подсветку
+//        HighlightSpot(false);
+//        TowerManager.Instance.HidePreview();
+//    }
+
+//    private void OnMouseDown()
+//    {
+//        Debug.Log("Mouse clicked!");
+//        if (!isOccupied && TowerManager.Instance.HasSelectedTower())
 //        {
-//            Debug.LogWarning("No tower selected or invalid spot!");
+//            // Логика размещения башни обрабатывается в TowerManager
+//            TowerManager.Instance.TryPlaceTower();
+//        }
+//    }
+
+//    private void HighlightSpot(bool highlight)
+//    {
+//        if (spotRenderer != null)
+//        {
+//            Debug.Log("Color changed");
+//            // Можно настроить цвет подсветки здесь
+//            //spotRenderer.color = highlight ? Color.green, Color.a = 90 : Color.white, Color.a = 90;
+//            if (highlight)
+//            {
+//                Color color = Color.green;
+//                color.a = 0.3f;
+//                spotRenderer.color = Color.green;
+//            }
+//            else
+//            {
+//                Color color = Color.white;
+//                color.a = 0.3f;
+//                spotRenderer.color = color;
+//            }
 //        }
 //    }
 //}
@@ -50,31 +90,22 @@ public class TowerSpot : MonoBehaviour
 
     private void OnMouseEnter()
     {
-        Debug.Log("Mouse entered TowerSpot");
         if (!isOccupied && TowerManager.Instance.HasSelectedTower())
         {
-            // Подсвечиваем место для размещения
             HighlightSpot(true);
         }
-        // Показываем превью выбранной башни
-        TowerManager.Instance.ShowPreview(transform.position);
     }
 
     private void OnMouseExit()
     {
-        Debug.Log("Mouse exit TowerSpot");
-        // Убираем подсветку
         HighlightSpot(false);
-        TowerManager.Instance.HidePreview();
     }
 
     private void OnMouseDown()
     {
-        Debug.Log("Mouse clicked!");
         if (!isOccupied && TowerManager.Instance.HasSelectedTower())
         {
-            // Логика размещения башни обрабатывается в TowerManager
-            TowerManager.Instance.TryPlaceTower();
+            TowerManager.Instance.TryPlaceTower(transform.position, this);
         }
     }
 
@@ -82,21 +113,12 @@ public class TowerSpot : MonoBehaviour
     {
         if (spotRenderer != null)
         {
-            Debug.Log("Color changed");
-            // Можно настроить цвет подсветки здесь
-            //spotRenderer.color = highlight ? Color.green, Color.a = 90 : Color.white, Color.a = 90;
-            if (highlight)
-            {
-                Color color = Color.green;
-                color.a = 0.3f;
-                spotRenderer.color = color;
-            }
-            else
-            {
-                Color color = Color.white;
-                color.a = 0.3f;
-                spotRenderer.color = color;
-            }
+            // Меняем цвет подсветки в зависимости от флага
+            Color color = highlight ? Color.green : Color.white;
+            color.a = 0.3f; // Полупрозрачность
+            spotRenderer.color = color;
         }
     }
 }
+
+
